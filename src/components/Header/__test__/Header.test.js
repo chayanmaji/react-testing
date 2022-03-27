64ed1992ed1c581e010657c10ef61f610ebd3737
Chayan Maji <chayanmaji@gmail.com>
2022-03-27 13:11:42 -0400

    Test TodoFooter

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,14 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-test("Header should render the text that passed as title", async ()=>{
-    render(<Header title={"Hello brother"}/>)
-    const headerElement = screen.getByText(/hello brother/i)
-    expect(headerElement).toBeInTheDocument();
+describe("Header Test",()=>{
+    test("Header should render the text that passed as title", async ()=>{
+        render(<Header title={"Hello brother"}/>)
+        const headerElement = screen.getByText(/hello brother/i)
+        expect(headerElement).toBeInTheDocument();
+    })
+    
+    test("Header should render the text that passed as title and tested by getByRole",async ()=>{
+        render(<Header title={"Hello World"}/>);
+        const headerElement = screen.getByRole("heading",{name:"Hello World"});
+        expect(headerElement).toBeInTheDocument();
+    })
+
+    
 })
 
-test("Header should render the text that passed as title and tested by getByRole",async ()=>{
-    render(<Header title={"Hello World"}/>);
-    const headerElement = screen.getByRole("heading",{name:"Hello World"});
-    expect(headerElement).toBeInTheDocument();
-})
